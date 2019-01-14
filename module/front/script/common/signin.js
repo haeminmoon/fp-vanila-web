efda65f37f669f8c38748ebedfe5690650c81a48
yunashin <yuna4573@naver.com>
2019-01-14 17:15:33 +0900

    Edit sign

--- module/front/script/common/signin.js
+++ module/front/script/common/signin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:module/front/script/signin.js
 // !function () {
 //     const do = {};
 
@@ -6,9 +5,9 @@
 //             do
 //     }
 // }();
-=======
-!function() {
+
+!function () {
     global.Signin = {
-    }; 
-} ();
->>>>>>> ca61d7cb65f19727e8ed902d281893ba1ad0da97:module/front/script/common/signin.js
+    };
+}();
+
