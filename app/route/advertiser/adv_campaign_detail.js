48ef8b5b8982bfafd8482cbc592162c126ef0cf3
yunashin <yuna4573@naver.com>
2019-01-17 16:34:05 +0900

    Modify + Add ..

--- app/route/advertiser/adv_campaign_detail.js
+++ app/route/advertiser/adv_campaign_detail.js
@@ -39,51 +39,72 @@ app.get('/advertiser/adv_campaign_detail', (req, res) => {
                             <a class="modify">수정하기</a>
                         </h2>
                         <table>
-                            <caption>캠페인 참여 인플루언서 게시판</caption>
-                            <colgroup>
-                                <col style="width: 70px">
-                                <col style="width: 180px">
-                                <col style="width: 150px">
-                                <col style="width: 250px">
-                            </colgroup>
-                            <thead>
-                                <tr>
-                                    <th scope="col" class="num">NO</th>
-                                    <th scope="col" class="infu_name">인플루언서 이름</th>
-                                    <th scope="col" class="price ud">판매 금액</th>
-                                    <th scope="col" class="date_camp ud">캠페인 선택일자</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="num">1</td>
-                                    <td class="infu_name">
-                                    <img src="http://www.newsfreezone.co.kr/news/photo/201808/73247_61537_3656.jpg" alt="인플루언서 사진"/>
-                                        <p>나리지연</p>
-                                    </td>
-                                    <td class="price">22,500원</td>
-                                    <td class="date_camp">2018-12-12 ~ 2019-01-01</td>
-                                </tr>
-                                <tr>
-                                    <td class="num">2</td>
-                                    <td class="infu_name">
-                                        <img src="http://www.newsfreezone.co.kr/news/photo/201808/73247_61537_3656.jpg" alt="인플루언서 사진"/>
-                                        <p>나리지연</p>
-                                    </td>
-                                    <td class="price">22,500원</td>
-                                    <td class="date_camp">2018-12-12 ~ 2019-01-01</td>
-                                </tr>
-                                <tr>
-                                    <td class="num">3</td>
-                                    <td class="infu_name">
-                                        <img src="http://www.newsfreezone.co.kr/news/photo/201808/73247_61537_3656.jpg" alt="인플루언서 사진"/>
-                                        <p>나리지연</p>
-                                    </td>
-                                    <td class="price">22,500원</td>
-                                    <td class="date_camp">2018-12-12 ~ 2019-01-01</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <caption>캠페인 리스트 전체 인플루언서 게시판</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col" class="num">NO</th>
+                                <th scope="col" class="infu_name">인플루언서 이름</th>
+                                <th scope="col" class="price ud">판매 금액</th>
+                                <th scope="col" class="price ud">판매 수량</th>
+                                <th scope="col" class="date_camp ud">캠페인 선택일자</th>
+                                <th scope="col" class="camp_state ud">상태</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="tr_on">
+                                <td class="num">1</td>
+                                <td class="infu_name">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrL8BHB7O4aPxelRrBvYb4zr2RIx_CU8ppu8gpE4HRgwT2Ma8" alt="인플루언서 사진"/>
+                                    <p>나리지연</p>
+                                </td>
+                                <td class="price">22,500원</td>
+                                <td class="price_num">15</td>
+                                <td class="date_camp">2018-12-12 10:10:10</td>
+                                <td class="camp_state">
+                                    <span class="check1">판매중</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="num">2</td>
+                                <td class="infu_name">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrL8BHB7O4aPxelRrBvYb4zr2RIx_CU8ppu8gpE4HRgwT2Ma8" alt="인플루언서 사진"/>
+                                    <p>나리지연</p>
+                                </td>
+                                <td class="price">22,500원</td>
+                                <td class="price_num">20</td>
+                                <td class="date_camp">2018-12-12 10:10:10</td>
+                                <td class="camp_state">
+                                    <span class="check1">판매중</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="num">3</td>
+                                <td class="infu_name">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrL8BHB7O4aPxelRrBvYb4zr2RIx_CU8ppu8gpE4HRgwT2Ma8" alt="인플루언서 사진"/>
+                                    <p>나리지연</p>
+                                </td>
+                                <td class="price">22,500원</td>
+                                <td class="price_num">40</td>
+                                <td class="date_camp">2018-12-12 10:10:10</td>
+                                <td class="camp_state">
+                                    <span class="check1">판매중</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="num">4</td>
+                                <td class="infu_name">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrL8BHB7O4aPxelRrBvYb4zr2RIx_CU8ppu8gpE4HRgwT2Ma8" alt="인플루언서 사진"/>
+                                    <p>나리지연</p>
+                                </td>
+                                <td class="price">22,500원</td>
+                                <td class="price_num">145</td>
+                                <td class="date_camp">2018-12-12 10:10:10</td>
+                                <td class="camp_state">
+                                    <span class="check1">판매중</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                     </div>
                 </div>
             </div>
