ef0131b4e989f28a149fbc774bb6394b100e087e
유주현 <gus6359@gmail.com>
2019-01-22 16:56:05 +0900

    캠페인 수정 화면 html 및 css 구성

--- app/route/advertiser/adv_campaign_modify.js
+++ app/route/advertiser/adv_campaign_modify.js
@@ -1,100 +1,144 @@
-app.get('/advertiser/adv_campaign_modidfy', async (req, res) => {
+app.get('/advertiser/adv_campaign_modify', async (req, res) => {
     if (!req.session.user || req.session.user.auth !== 'advertiser') return res.redirect('/common/signin');
     
     let [campaign] = await QUERY`SELECT * FROM campaign WHERE id = ${req.query.id}`;
     res.send(TMPL.layout.hnmf({
         css: `
             <link rel="stylesheet" href="/front/css/advertiser/adv_common_campaign.css" />
-            <link rel="stylesheet" href="/front/css/advertiser/adv_campaign_detail.css" />
+            <link rel="stylesheet" href="/front/css/advertiser/adv_campaign_modify.css" />
         `,
         header: TMPL.layout.advHeader(),
         nav: TMPL.layout.advNav(),
         main: `
             <div id="main">
-                <label>캠페인 명 </label><input type="text" class="campaign_title" value=${campaign.name}>
-                <div class="date_wrap">
-                    <p>캠페인 일정</p>
-                    <li>
-                        <label>신청기간 </label>
-                        <input type="date" class="date_start" name="apply_start_date" target="apply_end_date" value=${convertDate2String(campaign.apply_start_date)}><em>~</em><input type="date" name="apply_end_date" class="date_end" min=${convertDate2String(campaign.apply_start_date)} value=${convertDate2String(campaign.apply_end_date)}>
-                    </li>
-                    <li>
-                        <label>포스팅 기간 </label>
-                        <input type="date" class="date_start" name="post_start_date" target="post_end_date" value=${convertDate2String(campaign.post_start_date)}><em>~</em><input type="date" name="post_end_date" class="date_end" min=${convertDate2String(campaign.post_start_date)} value=${convertDate2String(campaign.post_end_date)}>
-                    </li>
-                    <li>
-                        <label>발표일 </label>
-                        <input type="date" name="notice_date" class="notice_date" value=${convertDate2String(campaign.notice_date)}>
-                    </li>
-                </div>
-                <div class="main_img_wrap">
-                    <p>대표이미지</p>
-                    <img src="${campaign.img}?${new Date()}" class="main_img non_modified" name="main_img" file_name=${getFileName(campaign.img)} sub_order=0 height="200" width="200">
-                    <input type="file" target="${getFileName(campaign.img)}" accept=".jpg, .jpeg, .png" class="img_url" name="img_url">
-                </div>
-                <div class="sub_img_wrap">
-                    <p>서브이미지</p>
-                    ${!campaign.sub_img ? writeHtmlEmtySubImg([]) : writeHtmlSubImg(campaign.sub_img)}
-                </div>
-                <div class="info_wrap">
-                    <p>상세정보 수정</p>
-                    <label>SNS 타입 </label>
-                    <select name="sns_type" class="sns_type">
-                        <option value="">SNS</option>
-                        <option value="instagram">인스타그램</option>
-                        <option value="facebook">페이스북</option>
-                        <option value="blog">블로그</option>
-                        <option value="youtube">유튜브</option>
-                    </select>
-                    <label>성별 </label>
-                    <select name="sex" class="sex">
-                        <option value="">성별</option>
-                        <option value="man">남자</option>
-                        <option value="woman">여자</option>
-                    </select>
-                    <label>연령대 </label>
-                    <select name="age" class="age">
-                        <option value="">연령대</option>
-                        <option value="teens">10대</option>
-                        <option value="twenties">20대</option>
-                        <option value="thirties">30대</option>
-                        <option value="fourties">40대</option>
-                    </select>
-                    <label>카테고리 </label>
-                    <select name="category" class="category">
-                        <option value="">선택</option>
-                        <option value="fashion">Fashion</option>
-                        <option value="cosmetic">Cosmetic</option>
-                        <option value="living">Living</option>
-                        <option value="food">Food</option>
-                    </select>
-
-                    <div class="input_wrap">
-                        <label for="camp_description" class="camp_description_wrap">캠페인 설명</label>
-                        <div class="wrap_right camp_description_wrap">
-                            <textarea name="camp_description" cols="40" rows="4" class="camp_description">${campaign.info.camp_description}</textarea>
+                <div class="container">
+                    <div class="breadcrumbs">
+                        <a>홈</a>
+                        <span>캠페인 리스트</span>
+                    </div>
+                    <div class="wrap">
+                        <div class="input_wrap">
+                            <label>캠페인 명<sup>*</sup></label>
+                            <div class="campaign_wrap">
+                                <input type="text" class="campaign_title" value=${campaign.name}>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>캠페인 일정<sup>*</sup></label>
+                            <div class="date_wrap">
+                                <ul>
+                                    <li>
+                                        <label>신청기간</label>
+                                        <input type="date" class="date_start" name="apply_start_date" target="apply_end_date" value=${convertDate2String(campaign.apply_start_date)}>
+                                        <em>~</em>
+                                        <input type="date" name="apply_end_date" class="date_end" min=${convertDate2String(campaign.apply_start_date)} value=${convertDate2String(campaign.apply_end_date)}>
+                                    </li>
+                                    <li>
+                                        <label>포스팅 기간 </label>
+                                        <input type="date" class="date_start" name="post_start_date" target="post_end_date" value=${convertDate2String(campaign.post_start_date)}>
+                                        <em>~</em>
+                                        <input type="date" name="post_end_date" class="date_end" min=${convertDate2String(campaign.post_start_date)} value=${convertDate2String(campaign.post_end_date)}>
+                                    </li>
+                                    <li>
+                                        <label>발표일 </label>
+                                        <input type="date" name="notice_date" class="notice_date" value=${convertDate2String(campaign.notice_date)}>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>대표이미지<sup>*</sup></label>
+                            <div class="main_img_wrap">
+                                <img src="${campaign.img}?${new Date()}" class="main_img non_modified" name="main_img" 	file_name=${getFileName(campaign.img)} sub_order=0>
+                                <input type="file" target=${getFileName(campaign.img)} accept=".jpg, .jpeg, .png" class="img_url" name="img_url">
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>서브이미지<sup>*</sup></label>
+                            <div class="sub_img_wrap">
+                                <div>
+                                    ${!campaign.sub_img ? writeHtmlEmtySubImg([]) : writeHtmlSubImg(campaign.sub_img)}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="input_wrap">
-                        <label for="mission_description" class="mission_description_wrap">미션 설명</label>
-                        <div class="wrap_right mission_description_wrap">
-                            <textarea name="mission_description" cols="40" rows="4" class="mission_description">${campaign.info.mission_description}</textarea>
+                    <div class="info_wrap">
+                        <h2>상세정보 수정</h2>
+                        <div class="input_wrap">
+                            <label>SNS 타입<sup>*</sup></label>
+                            <div class="sns_wrap">
+                                <select name="sns_type" class="sns_type">
+                                <option value="">SNS</option>
+                                <option value="instagram">인스타그램</option>
+                                <option value="facebook">페이스북</option>
+                                <option value="blog">블로그</option>
+                                <option value="youtube">유튜브</option>
+                            </select>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>성별<sup>*</sup></label>
+                            <div class="sex_wrap">
+                                <select name="sex" class="sex">
+                                    <option value="">성별</option>
+                                    <option value="man">남자</option>
+                                    <option value="woman">여자</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>연령대<sup>*</sup></label>
+                            <div class="age_wrap">
+                                <select name="age" class="age">
+                                    <option value="">연령대</option>
+                                    <option value="teens">10대</option>
+                                    <option value="twenties">20대</option>
+                                    <option value="thirties">30대</option>
+                                    <option value="fourties">40대</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label>카테고리<sup>*</sup></label>
+                            <div class="category_wrap">
+                                <select name="category" class="category">
+                                    <option value="">선택</option>
+                                    <option value="fashion">Fashion</option>
+                                    <option value="cosmetic">Cosmetic</option>
+                                    <option value="living">Living</option>
+                                    <option value="food">Food</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label for="camp_description" class="camp_description_wrap">캠페인 설명<sup>*</sup></label>
+                            <div class="camp_description_wrap">
+                                <textarea name="camp_description" cols="40" rows="4" class="camp_description">${campaign.info.camp_description}</textarea>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label for="mission_description" class="mission_description_wrap">미션 설명<sup>*</sup></label>
+                            <div class="mission_description_wrap">
+                                <textarea name="mission_description" cols="40" rows="4" class="mission_description">${campaign.info.mission_description}</textarea>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label for="benefit_description" class="benefit_description_wrap">보상 설명<sup>*</sup></label>
+                            <div class="benefit_description_wrap">
+                                <textarea name="benefit_description" cols="40" rows="4" class="benefit_description">${campaign.info.benefit_description}</textarea>
+                            </div>
+                        </div>
+                        <div class="input_wrap">
+                            <label for="hash_tag" class="hash_tag_wrap">해시 태그<sup>*</sup></label>
+                            <div class="hash_tag_wrap">
+                                <input type="text" name="hash_tag" class="hash_tag" value=${campaign.info.hash_tag}>
+                            </div>
                         </div>
                     </div>
-                    <div class="input_wrap">
-                        <label for="benefit_description" class="benefit_description_wrap">보상 설명</label>
-                        <div class="wrap_right benefit_description_wrap">
-                            <textarea name="benefit_description" cols="40" rows="4" class="benefit_description">${campaign.info.benefit_description}</textarea>
-                        </div>
+                    <div class="btn_box">
+                        <button class="modify_btn">수정</button>
+                        <button class="cancel" name=${req.query.id}>취소</button>
                     </div>
-                    <div class="input_wrap">
-                        <label for="hash_tag" class="hash_tag_wrap">해시 태그</label>
-                        <div class="wrap_right hash_tag_wrap">
-                            <input type="text" name="hash_tag" class="hash_tag" value=${campaign.info.hash_tag}>
-                        </div>
-                    </div>
-                    <button class="modify_btn">수정</button>
-                    <button class="cancel" name=${req.query.id}>취소</button>
                 </div>
             </div>
         `,
@@ -104,7 +148,6 @@ app.get('/advertiser/adv_campaign_modidfy', async (req, res) => {
             <script>
                 go('.cancel', $, AdvCampaignModify.Do.cancelModify);
                 go('.modify_btn', $, AdvCampaignModify.Do.modify);
-                go('#main', $, AdvCampaignModify.Do.clickPlusButton);
                 go('#main', $, AdvCampaignModify.Do.readyImage);
                 go('#main', $, AdvCampaignModify.Do.setMinDate);
                 go({
@@ -187,10 +230,12 @@ const writeHtmlSubImg = subImgArr => {
     return go(
         subImgArr,
         map(a => html`
-            <img src=${a}?${new Date()} class="sub_img non_modified" name="sub_img" file_name=${getFileName(a)} sub_order=${order++} height="200" width="200">
-            <input type="file" target="${getFileName(a)}" accept="image/*" class="img_url" name="img_url">
+            <div class="sub_img_wrap">
+                <img src=${a}?${new Date()} class="sub_img non_modified" name="sub_img" file_name=${getFileName(a)} sub_order=${order++}>
+                <input type="file" target="${getFileName(a)}" accept="image/*" class="img_url" name="img_url">
+            </div>
         `),
-        b => b + writeHtmlEmtySubImg(b)
+        b => b.join('') + writeHtmlEmtySubImg(b)
     )
 }
 
@@ -199,12 +244,11 @@ const writeHtmlEmtySubImg = subImgArr => go(
     _ => {
         let result = "";
         for (let i = subImgArr.length; i < 3; i++) {
-            result += `
-                <button class="sub_img_plus" index=${i}>+</button>
-                <div class="sub_img_wrap_${i} hidden">
-                    <img src=null class="sub_img non_modified" name="sub_img" file_name="campaign_subImage_${i+1}" sub_order=${i} height="200" width="200">
-                    <input type="file" target="campaign_subImage_${i+1}" accept=".jpg, .jpeg, .png" class="img_url" name="img_url">
-                </div>
+            result += html`
+            <div class="sub_img_wrap">
+                <img src=null class="sub_img non_modified" name="sub_img" file_name="campaign_subImage_${i+1}" sub_order=${i}>
+                <input type="file" target="campaign_subImage_${i+1}" accept=".jpg, .jpeg, .png" class="img_url" name="img_url">
+            </div>
             `
         }
         return result;
